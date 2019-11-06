d492e76a3808590eee557343b2d1b17cb7dec7dc
everaldo2019 <everaldoeverdosa@gmail.com>
2019-11-06 16:45:44 -0300

    programa funcionando

--- calcula distancia.c
+++ calcula distancia.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 //função que calcula a distancia
-float calculaDistancia (int x1,inty1,int x2,int y2){
+float calculaDistancia (int x1,int y1,int x2,int y2){
 return sqrt (pow (x2-x1,2)+pow(y2-y1,2));
 }
 //main
